f0920f3c95afa0f5359598f6c5c6b7eebcc8a5eb
Ian-theeasy <ianyh@outlook.com>
2015-06-19 20:08:34 +0000

    add coed

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,9 @@
+class PageController < ApplicationController
+	def welcome
+  		@issues  = [ { title: "Ruby lovers", comments: "4" }, { title: "Rails and Laravel", comments: "5" } ]
+  	end
+
+	def about
+	end
+
+end
